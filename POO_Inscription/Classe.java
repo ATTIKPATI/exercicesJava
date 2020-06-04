e369bd77359ab824e096fd8ebbe56ae0b871d4ed
ATTIKPATI <64203534+ATTIKPATI@users.noreply.github.com>
2020-06-04 11:59:24 +0000

    Add files via upload

--- POO_Inscription/Classe.java
+++ POO_Inscription/Classe.java
@@ -1,10 +1,24 @@
 public class Classe implements IInscription{
 
+   //Auto généré
     private int id;
     private String nom;
 
-   
 
+    //Constructeur
+    public Classe(){
+
+    }
+
+    public Classe(String nom){
+       this.nom=nom;
+   }
+
+
+   //Getters and Setters
+
+
+   //redefinition de la methode affiche()
 
     @Override
     public String affiche(){
---
+++ POO_Inscription/Main.java
@@ -0,0 +1,40 @@
+public static void main(String args[]){
+Scanner clavier=new Scanner(System.in);
+Service service=new Service ();
+String choix;
+do{
+   System.out.println("1-Ajouter Classe");
+   System.out.println("2-Lister Classe");
+   System.out.println("3-Inscrire Etudiant");
+   System.out.println("4-Lister Etudiant");
+   System.out.println("5-Affecter Professeur");
+   System.out.println("6-Lister Professeur");
+   System.out.println("7-Quitter");
+   System.out.println("Faites votre choix");
+   choix=Integer.parseInt(clavier.nextLine());
+
+switch(choix){
+      case "1":
+         System.out.println("Entrer le Libelle d'une Classe");
+         String libelle=clavier.nextline();
+         Classe cl=new Classe(libelle);
+        /*cl.setLibelle(libelle);*/
+         service.ajouterClasse(cl);
+     break;
+     case 2:
+        Sytem.out.println("------------------")
+        service.listerClasse();
+        System.out.println("-----------------")
+        break;
+    case 2:
+        Sytem.out.println("------------------")
+        service.inscrireEtudiant();
+        System.out.println("-----------------")
+        break;
+    case 2:
+        Sytem.out.println("------------------")
+        service.listerEtudiant();
+        System.out.println("-----------------")
+        break;
+}}
+}
