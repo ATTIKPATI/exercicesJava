4f8e1de1e4f1457534417add8fe504c1fe5925a8
ATTIKPATI <64203534+ATTIKPATI@users.noreply.github.com>
2020-06-04 12:06:55 +0000

    Add files via upload

---
+++ POO_Inscription/Service.java
@@ -0,0 +1,68 @@
+public class Service {
+
+     //Taille Reservation :nbre Cellule du Tableau
+      final int TAILLE=10;
+    //Taille Réélle :nbre element du Tableau
+      private int nbreClasse=0;//nbre Classe dans le tableau ,
+                               //mais aussi la position d'insertion
+      private int nbrePersonne=0;
+    //Declaration des Tableaux
+    private Classe tabclasse[];
+    private Personne [] tabPersonne;
+
+    public Service(){
+        //Creation du Tableau
+        tabclasse=new Classe[TAILLE];
+        tabPersonne=new Personne[TAILLE];
+        
+
+    }
+
+
+    public void ajouterClasse(Classe classe){
+        
+            tabclasse[nbreDeClasse]=classe;
+            nbreDeClasse++;
+        
+        
+    }
+
+    public void listerClasse(){
+        for(int i=0;i<nbreDeClasse;i++){
+            System.out.println(tabclasse[i].affiche());
+        }
+        
+    }
+    public void inscrireEtudiant(Etudiant et){
+        
+            tabPersonnes[nbreDePersonne]=et;
+            nbreDePersonnes++;
+        
+        
+    }
+    public void listerEtudiant(){
+        for(int i=0;i<nbreDePersonne;i++){
+            System.out.println(tabPersonnes[i]et.affiche());
+        }
+        
+    }
+
+    public void  ajouterPersonne(Personne pers){
+
+    }
+
+    public void listerEudiantParClasse(){
+       
+        for(int i=0;i<nbreEtudiant;i++){
+            System.out.println(tabclasse[i].affiche());
+        }
+        
+        
+    }
+
+    //Uc 
+
+
+
+    
+}
